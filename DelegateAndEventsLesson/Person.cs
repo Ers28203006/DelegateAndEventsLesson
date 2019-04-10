739826d459633fd849da30b2a809bb54925f0c67
Ers28203006 <ersyn_sultanbaev@mail.ru>
2019-04-10 22:52:10 +0600

    Добавьте файлы проекта.

---
+++ DelegateAndEventsLesson/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DelegateAndEventsLesson
+{
+    public delegate void PropertyeventHandler();
+    class Person : IPropertychanged
+    {
+        private string _name;
+        private string _surname;
+
+        public event PropertyeventHandler Propertychanged;
+        
+        public Person(string name, string surname)
+        {
+            _name = name;
+            _surname = surname;
+            Propertychanged = IdentityUpdate;
+            IdentityDate();
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                _surname = value;
+                if (_surname == value)
+                {
+                    Propertychanged();
+                }
+            }
+        }
+
+
+        public void IdentityUpdate()
+        {
+            Console.WriteLine($"*******************\n" +
+                $"Смена девичьей фамилии на \n" +
+                $"Имя: {_name}\nФамилия:{_surname}");
+
+        }
+
+        public void IdentityDate()
+        {
+            Console.WriteLine($"Имя: {_name}\nФамилия:{_surname}");
+
+        }
+    }
+}
---
+++ DelegateAndEventsLesson/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DelegateAndEventsLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Тема: Делегаты, события.
+             * 1. Изменение свойств объекта класса
+             * Создать интерфейс, который уведомляет пользователя программы об изменении значений свойства в классе 
+             * (в котором реализуется данный интерфейс). Сигнатура интерфейса:
+                  
+                    * interface iPropertychanged
+                    * {
+                      * event PropertyeventHandler Propertychanged;
+                    * }
+             
+             * Делегат имеет следующий вид:
+                    
+                    * public delegate void PropertyeventHandler (object sender, PropertyeventArgs e);
+                    
+                    * где, в качестве дополнительного параметра (Property­EventArgs) выступает имя изменённого свойства. 
+             * В методе Main() проверить работоспособность созданного интерфейса.*/
+
+            Person woman = new Person("Анастасия", "Шредер");
+            woman.Surname = "Акимова";
+            Console.ReadLine();
+        }
+    }
+}
